2668b2fec538ff98f1bfc122ec724a0b1e2a42c4
RsMacAir <51xiuxian@sina.com>
2017-03-16 06:38:37 +0800

    users can see their orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
+  def index
+    @orders = Order.all
+  end
+
   def create
     @order = Order.new(order_params)
     @order.user = current_user
